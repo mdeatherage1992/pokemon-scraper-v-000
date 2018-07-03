522f6b849370431fcd28ead12155d70472ef4743
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-03 16:06:03 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+   attr_accessor :id, :name,:type,:db
+   @@all = []
+  def initialize(id:,name:,type:,db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+end
+
+def self.save(id, name, type, db)
+  database_connection.execute("INSERT INTO  (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
+end
+
+
 end
